e2db0154c9f2654cd0609bd5673a180b6084dab2
Daniel L. Alves <daniel.leonssio@hotmail.com>
2024-01-30 18:55:08 -0300

    Inserindo input, condicionais if e else.

--- jogo-advinhacao/game.cpp
+++ jogo-advinhacao/game.cpp
@@ -5,12 +5,35 @@ int main() -> função principal, onde será hospedado o programa
 std -> indicador de função padrão do c++
 cout -> comando para imprimir um output
 endl -> Fim de linha (endline). Semelhante ao breakrow, faz com que uma linha seja pulada ao término da sentença.
+int <var> -> declara uma variável do tipo inteiro
+cin -> Faz com que o usuário consiga "inputar" um valor.
 */
 
 #include<iostream>
 
 int main (){
     std::cout << "Bem vindo ao jogo da advinhação." << std::endl;
+
+    int numero_secreto = 42;
+    std::cout << "O numero secreto é " << numero_secreto << ". Não conte para ninguém!" << std::endl;
+
+    int chute;
+    std::cout << "Você deverá acertar o número escolhido pela máquina." << std::endl;
+    std::cin >> chute;
+    std::cout << "Você chutou o número " << chute << "." << std::endl;
+
+    if (chute == numero_secreto){
+        std::cout << "Parabéns, você acertou o número secreto!" << std::endl;
+    }
+
+    else if (chute > numero_secreto){
+        std::cout << "O chute foi maior do que o número escolhido. Tente novamente" << std::endl;
+    }
+
+    else if (chute < numero_secreto){
+        std::cout << "O chute foi menor do que o número escolhido. Tente novamente" << std::endl;
+    }
+
 }
 
 /*
