8f086b1d818e6a5a4cd73a41ab13c191c3d9d475
Daniel L. Alves <daniel.leonssio@hotmail.com>
2024-02-03 16:24:11 -0300

    Finalizando o jogo

--- jogo-forca/game.cpp
+++ jogo-forca/game.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 #include <string>
 #include <cctype> // Para usar a função "tolower()"
+#include <map>
+#include <vector>
 
 using namespace std;
 
-const string PALAVRA_SECRETA = "Melancia";
+const string PALAVRA_SECRETA = "MELANCIA";
+map <char, bool> chutou;
+vector <char> chutes_errados;
 
 bool verifica_letra(char chute){
 
     for (char letra : PALAVRA_SECRETA){
-        if(tolower(chute) == tolower(letra)){
+        if(toupper(chute) == toupper(letra)){
+            return true;
+        }
+    }
+    return false;
+}
+
+bool nao_acertou(){
+    for (char letra: PALAVRA_SECRETA){
+        if (!chutou[letra]){
             return true;
         }
     }
@@ -20,29 +33,33 @@ int main (){
 
     cout << "Bem vindo ao jogo da forca. Você terá 6 chances para descobrir a palavra secreta.\n" << endl;
 
-    cout << PALAVRA_SECRETA << endl;
-
-    bool nao_acertou = true;
     bool nao_enforcou = true;
-
-    string underscore_PALAVRA_SECRETA = "";
-
-    for (int i = 1; i <= PALAVRA_SECRETA.length(); i += 1) {
-        underscore_PALAVRA_SECRETA.append("_ ");
-    }
-
-    cout << underscore_PALAVRA_SECRETA << endl;
 
     int vidas = 6;
 
-    while (nao_acertou && nao_enforcou){
+    while (nao_acertou() && nao_enforcou){
+
+        for (char letra: PALAVRA_SECRETA){
+            if (chutou[letra]){
+                cout << letra << " ";
+            }
+            else {
+                cout << "_ ";
+            }
+        }
+        cout << endl;
         
         cout << "Tente advinhar uma letra presente na palavra secreta." << endl;
         char chute;
         cin >> chute;
+        cout << endl;
+
+        char chute_upper = toupper(chute);
+
+        chutou[chute_upper] = true;
 
         if (verifica_letra(chute)){
-            cout << "Seu chute está correto!" << endl;            
+            cout << "Seu chute está correto!" << endl;
         }
 
         else {
@@ -55,9 +72,13 @@ int main (){
             
             else{
                 cout << "Seu chute está errado. Atualmente você possui " << vidas << " vidas." << endl;
+                chutes_errados.push_back(chute_upper);
             }
-
+        cout << endl;
         }
-        
+        for (char letra: chutes_errados){
+            cout << "\nChutes errados: " << letra << " ";
+        }
+        cout << endl;
     }
 }
